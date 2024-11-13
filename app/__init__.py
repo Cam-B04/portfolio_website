662d9499e38fe0638972bcde8859a7048c6cd269
Camille Besombes <camillebesombespro@gmail.com>
2024-11-13 12:45:07 -0500

    updated footer

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__, static_folder=Config.STATIC_FOLDER, template_folder=Config.TEMPLATES_FOLDER)
+app = Flask(__name__, static_folder=Config.STATIC_FOLDER,
+            template_folder=Config.TEMPLATES_FOLDER)
 app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,28 @@
-from flask import render_template, url_for
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
 
+
 @app.route('/description')
 def description():
     return render_template('description.html', title='Description')
+
 
 @app.route('/projects')
 def projects():
     return render_template('projects.html', title='Projects')
 
+
 @app.route('/others')
 def others():
     return render_template('others.html', title='Others')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html', title='Contact')
-
