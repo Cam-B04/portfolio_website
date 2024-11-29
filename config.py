f088057cd7fafe94cb9db4eea2b9af0baa6e16c2
Camille Besombes <camillebesombespro@gmail.com>
2024-11-29 15:41:01 +0100

    first commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    SECRET_KEY = 'your_secret_key'
-    DEBUG = True
+    SECRET_KEY = '3f2b449ff08b78e886a3460e91d109cbc6231780d295c089674a0736c0ca5f80'
+    DEBUG = False
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
