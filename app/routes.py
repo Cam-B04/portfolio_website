ab6741cc9d9bcf4f4a73d47279daeee2f0054aa3
Camille Besombes <camillebesombespro@gmail.com>
2024-11-29 18:07:39 +0100

    modified the margins and clean some section that were useless

--- app/routes.py
+++ app/routes.py
@@ -30,11 +30,6 @@ def home():
     return render_template('home.html', title='Home', projects=last_projects)
 
 
-@app.route('/description')
-def description():
-    return render_template('description.html', title='Description')
-
-
 @app.route('/projects')
 def projects():
     return render_template('projects.html', title='Projects', projects=projs)
@@ -43,8 +38,3 @@ def projects():
 @app.route('/others')
 def others():
     return render_template('others.html', title='Others')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', title='Contact')
